d866339f18801766fa36724aaaae0ab36972535c
nudimannui4e <nudimannui4e@protonmail.com>
2022-08-07 05:56:56 +0300

    css

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+# render_template - for html
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('templates/index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8080)
