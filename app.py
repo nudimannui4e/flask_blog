34a9b15433f9655ecdc7831cccc47ae76a3d547d
nudimannui4e <nudimannui4e@protonmail.com>
2022-08-07 06:15:12 +0300

    sqlite init scheme

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
 # render_template - for html
+import sqlite3
+
+def get_db_connection():
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    return connection
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    connection = get_db_connection()
+    post = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    return render_template('index.html', posts=posts)
 
 
 if __name__ == '__main__':
     app.run(debug=True,port=8080)
+
